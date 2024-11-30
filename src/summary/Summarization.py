d3504a9ffbff17e18551f0a72fceecfa5035da18
gaeng02 <sondin144@gmail.com>
2024-11-30 21:37:47 +0900

    fixed ratio

--- src/summary/Summarization.py
+++ src/summary/Summarization.py
@@ -1,10 +1,14 @@
 from gensim.summarization.summarizer import summarize
 
-def summarize_text (text, _ratio = 0.1) :
-    return summarize(text, ratio = _ratio)
+def summarize_text (text) :
+    
+    length = len(text)
+    # 50~100 => 80
+    return summarize(text, ratio = min(1, 80 / length))
+
 
 if __name__ == "__main__":
 
     text = ""
     
-    print(summarize_text (text, 0.2))
+    # print(summarize_text (text, 0.2))
